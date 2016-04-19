d58e4f95b08d8f1c513886d360a584b8ef762a71
Junior <jljunior3@gmail.com>
2016-04-18 22:53:19 -0300

    correcao de coluna

--- database/migrations/2016_04_16_164134_create_clients_table.php
+++ database/migrations/2016_04_16_164134_create_clients_table.php
@@ -15,7 +15,7 @@ class CreateClientsTable extends Migration
         Schema::create('clients', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('responsable');
+            $table->string('responsible');
             $table->string('email');
             $table->string('phone');
             $table->text('address');
